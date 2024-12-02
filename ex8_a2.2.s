57cf963baba3df22c3971cea19d037dd0ad400c3
abduqodir2335 <abduqodir1838@gmail.com>
2024-12-02 16:22:35 +0500

    Update ex8_a2.2.s

--- ex8_a2.2.s
+++ ex8_a2.2.s
@@ -1,12 +1,12 @@
 .global _start
 _start:
 		
-	MOV R0, #10
+  MOV R0, #10
   MOV R1, #20
   CMP R0,R1
   MOVGT R2, #1
-	MOVLE R2,  #0
+  MOVLE R2,  #0
 	
-	LDR R3, =0x30000000
-	STR R2, [R3]
+  LDR R3, =0x30000000
+  STR R2, [R3]
 	
